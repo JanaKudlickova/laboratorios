2e0a4e0a3e5cc446d9779f9e1caf82627cc8af3b
JanaKudlickova <jana-kudlickova_student2021@wilder.school>
2022-03-19 14:56:54 +0100

    exercise 2 - calculated netto salary

--- lab-03/app.js
+++ lab-03/app.js
@@ -26,3 +26,42 @@ switch(product.type) {
 let precioDelCarito = total + IVA;
    
 console.log(precioDelCarito);
+
+
+//EXERCISE 2: Calcular sueldo neto
+const empleado = {
+    bruto: 26000,
+    hijos: 0,
+    pagas: 12
+}
+
+let retencion;
+
+if (empleado.bruto <= 12000) {
+    retencion = 0;
+} else if (empleado.bruto > 12000 && empleado.bruto <= 24000 && empleado.hijos == 0) {
+    retencion = empleado.bruto * 0.08;
+} else if (empleado.bruto > 12000 && empleado.bruto <= 24000 && empleado.hijos > 0) {
+    retencion = empleado.bruto * 0.06;
+} else if (empleado.bruto > 24000 && empleado.bruto <= 34000 && empleado.hijos == 0) {
+    retencion = empleado.bruto * 0.16;
+} else if (empleado.bruto > 24000 && empleado.bruto <= 34000 && empleado.hijos > 0) {
+    retencion = empleado.bruto * 0.14;
+} else if (empleado.bruto > 34000 && empleado.hijos == 0) {
+    retencion = empleado.bruto * 0.3;
+} else if (empleado.bruto > 34000 && empleado.hijos > 0) {
+    retencion = empleado.bruto * 0.28;
+};
+
+console.log(retencion);
+
+//Salario neto divido en 14 o 12 pagas
+let salarioNeto;
+
+if (empleado.pagas === 14) {
+    salarioNeto = (empleado.bruto - retencion) / 14;
+} else {
+    salarioNeto = (empleado.bruto - retencion) / 12;
+}
+
+console.log(salarioNeto);
