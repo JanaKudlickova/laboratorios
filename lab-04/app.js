9001eb8e97198fe5116f83e3d6815ab9be24e384
JanaKudlickova <jana-kudlickova_student2021@wilder.school>
2022-06-13 20:34:20 +0200

    added extra exercise

--- lab-04/app.js
+++ lab-04/app.js
@@ -60,3 +60,54 @@ document.getElementById("add-button").addEventListener("click", validSumResult);
 document.getElementById("subtract-button").addEventListener("click", validSubtractResult);
 document.getElementById("multiply-button").addEventListener("click", validMultiplyResult);
 document.getElementById("divide-button").addEventListener("click", validDivideResult);
+
+
+/*=========================================EXTRA: HOTEL RESERVATIONS==========================================*/
+
+//Get price according to the room category; extra 20 for spa
+
+const roomCategory = () => document.getElementById("room-category").value;
+const spa = () => document.getElementById("spa-selected").checked;
+ 
+const categoryCost = () => {
+    const extraSpa = spa() === true ? 20 : 0;
+    if (roomCategory() === "standard") {
+        return 100 + extraSpa;
+    } else if ( roomCategory() === "junior") {
+        return 120 + extraSpa;
+    } else {
+        return 150 + extraSpa;
+    }
+}
+
+//Cost increased or decreased by the type of the room
+const roomType = () => document.getElementById("room-type").value;
+
+const roomTypeCost = () => {
+    if (roomType() === "individual") {
+        return categoryCost() * 0.75; 
+    } else if (roomType() === "triple") {
+        return categoryCost() * 1.25;
+    } else {
+        return categoryCost();
+    }
+}
+
+//Multiply cost by number of nights
+const stayDuration = () => document.getElementById("nights-number").value;
+const costWithoutParking = () => parseInt(stayDuration()) * roomTypeCost();
+
+
+//Parking 10 euros per night
+const getParkingNights = () => document.getElementById("parking-nights").value;
+const parkingCost = () => parseInt(getParkingNights()) * 10;
+
+const totalCost = () => costWithoutParking() + parkingCost();
+
+
+
+const submitResult = () => {
+    document.getElementById("final-cost").innerHTML = totalCost();
+};
+
+document.getElementById("calculate").addEventListener("click", submitResult);
